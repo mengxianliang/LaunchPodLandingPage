174bd3d65618da962e309e5ec51c535888a31fe8
mengxianliang <mengxianliang@users.noreply.github.com>
2025-10-29 09:31:26 +0000

    deploy: 77ea98032f54a69c534e31e24d4b3ee045b8df8c

--- _next/static/_QyV1AXqI2NUAfKT_8xd_/_buildManifest.js
+++ _next/static/_QyV1AXqI2NUAfKT_8xd_/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/LaunchPodLandingPage//_next/:path+",
+        "destination": "/LaunchPodLandingPage/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
